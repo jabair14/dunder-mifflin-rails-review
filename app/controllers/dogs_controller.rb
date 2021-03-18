1e1c88b211074bf2890961be9da528a7b33bee6d
Jabair <jabairk@gmail.com>
2021-03-18 16:12:30 -0400

    stuck on dog index page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,9 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all 
+
+        # render :index 
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :dogs
+  resources :dogs, only: [:index, :show]
   resources :employees
+
+  # get '/dogs', to: "dogs#index", as: 'dogs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
